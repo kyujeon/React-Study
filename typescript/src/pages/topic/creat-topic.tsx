4ab5d0b1eb4593742c8480dc61b1da9a234b3fc8
kyu jeon <in8994@naver.com>
2025-11-20 14:19:51 +0900

    [25.11.20] 미리보기 세팅

--- typescript/src/pages/topic/creat-topic.tsx
+++ typescript/src/pages/topic/creat-topic.tsx
@@ -1,6 +1,7 @@
 import { AppTextEditor } from "@/components/common";
 import {
   Button,
+  Input,
   Select,
   SelectContent,
   SelectGroup,
@@ -12,8 +13,69 @@ import {
 } from "@/components/ui";
 
 import { Asterisk, Image, ImageOff } from "lucide-react";
+import { useRef, useState } from "react";
 
 function CreateTopic() {
+  const [title, setTitle] = useState<string | null>(null);
+  const [content, setContent] = useState();
+  const [category, setCategory] = useState<string | null>(null);
+  const [thumbnail, setThumbnail] = useState<File | string | null>(null);
+
+  // => File 타입의 원본 데이터를 받음
+  // => Supabase의 이미지만 관리하는 Storage에 전달받은 File을 저장 => URL 형식으로
+  // => Supabase 데이터베이스에 저장 (in topics 테이블의 thumbnail 칼럼)
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  // 파일 변화 감지 및 상태값 할당
+  const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setThumbnail(event.target.files?.[0] ?? null);
+
+    console.log(event.target.files);
+
+    event.target.value = "";
+  };
+
+  // 이미지 미리보기
+  const handleRenderPreview = () => {
+    if (typeof thumbnail === "string") {
+      return (
+        <img
+          src={thumbnail}
+          alt="@THUMBNAIL"
+          className="w-full aspect-video rounded-md"
+        />
+      );
+    } else if (thumbnail instanceof File) {
+      // thumbnail은 File 객체여야 한다.
+      // 예를들어, <input type=file">에서 사용자가 선택한 파일을 나타내는 객체이다.
+      // createObjectURL 매서드는 파일을 브라우저에서 사용할 수 잇는 임시 URL 로 변환된다. *(미리보기)
+      // 이 URL 은 해당 파일에 대한 참조를 제공하여 로컬에서만 요효하다. 즉, 이 URL 은 서버에서 접근할 수 없고, 사용자의 브라우저내에서만 유요하다.
+      // 변환된 URL을 이미지, 비디오, 오디오 등의 미디어 파일에서 사용할 수 있다.
+
+      return (
+        <img
+          src={URL.createObjectURL(thumbnail)}
+          alt="@THUMBNAIL"
+          className="w-full aspect-video rounded-md"
+        />
+      );
+    }
+
+    //썸네일이 설정되지 않은 경우에는 기본 이미지 아이콘을 보여준다. (기본값)
+    return (
+      <div className="w-full aspect-video flex items-center justify-center rounded-md bg-card">
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          onClick={() => fileInputRef.current?.click()}
+        >
+          <Image />
+        </Button>
+      </div>
+    );
+  };
+
   return (
     <main className="w-full flex-1 flex justify-center">
       <div className="w-full max-w-[1328px] h-full flex gap-6 py-6">
@@ -24,14 +86,26 @@ function CreateTopic() {
             <p className="font-semibold text-base">토픽 작성하기</p>
           </div>
           <Separator />
-          <div className="flex flex-col gap-1">
-            <div className="flex items-center gap-1">
-              <Asterisk size={14} className="text-[#FA6859]" />
-              <p className="text-neutral-400 text-base">본문</p>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-1">
+              <div className="flex items-center gap-1">
+                <Asterisk size={14} className="text-[#FA6859]" />
+                <p className="text-neutral-400 text-base">제목</p>
+              </div>
+              <Input
+                placeholder="토픽제목을 입력하세요."
+                className="h-15 placeholder:text-base px-3 font-semibold border-none"
+              />
             </div>
-            {/* BlockNote 텍스트 에디터 ui*/}
-            <div className="w-full h-screen">
-              <AppTextEditor />
+            <div className="flex flex-col gap-1">
+              <div className="flex items-center gap-1">
+                <Asterisk size={14} className="text-[#FA6859]" />
+                <p className="text-neutral-400 text-base">본문</p>
+              </div>
+              {/* BlockNote 텍스트 에디터 ui*/}
+              <div className="w-full h-screen">
+                <AppTextEditor />
+              </div>
             </div>
           </div>
         </div>
@@ -81,13 +155,21 @@ function CreateTopic() {
                 <p className="text-neutral-400 text-base">썸네일</p>
               </div>
               <div className="flex flex-col gap-2">
-                <div className="w-full aspect-video flex items-center justify-center rounded-md bg-card">
-                  <Button variant={"ghost"} size={"icon"}>
-                    <Image />
-                  </Button>
-                </div>
+                {handleRenderPreview()}
+                <Input
+                  type="file"
+                  accept="image/*"
+                  ref={fileInputRef}
+                  className="hidden"
+                  onChange={handleChangeFile}
+                />
+
                 {/* 썸네일 제거 버튼 */}
-                <Button variant={"secondary"} className="bg-card">
+                <Button
+                  variant={"secondary"}
+                  className="bg-card"
+                  onClick={() => setThumbnail(null)}
+                >
                   <ImageOff />
                   썸네일 제거
                 </Button>
